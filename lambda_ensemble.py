fd4d3cc4e55b2e0a1160c57619f2e26369714a3a
manchann <chl8273@naver.com>
2021-10-14 11:29:58 +0900

    change file name

--- lambda_ensemble.py
+++ lambda_ensemble.py
@@ -1,17 +1,21 @@
 import json
-import boto3
-
-BUCKET_NAME = 'imagenet-sample'
-
-s3 = boto3.resource('s3')
-batchsize = 32
 
 def lambda_handler(event, context):
-    bucket = s3.Bucket(BUCKET_NAME)
-    filenames = [file.key for file in bucket.objects.all() if len(file.key.split('/')[1]) > 1]
+    model_num = len(event)
+    batch_size = len(event[0]["result"])
+    result = []
+    for img_idx in range(batch_size):
+        acc = 0
+        for model_idx in range(model_num):
+            acc += event[model_idx]["result"][img_idx][2]
+        acc /= model_num
+        res = []
+        res.append(event[0]["result"][img_idx][0])
+        res.append(event[0]["result"][img_idx][1])
+        res.append(acc)
+
+        result.append(res)
 
     return {
-        'file_list': filenames,
-        'batchsize': batchsize
+        'result': result
     }
-
--- lambda_invoker.py
+++ lambda_invoker.py
@@ -1,21 +1,17 @@
 import json
+import boto3
+
+BUCKET_NAME = 'imagenet-sample'
+
+s3 = boto3.resource('s3')
+batchsize = 32
 
 def lambda_handler(event, context):
-    model_num = len(event)
-    batch_size = len(event[0]["result"])
-    result = []
-    for img_idx in range(batch_size):
-        acc = 0
-        for model_idx in range(model_num):
-            acc += event[model_idx]["result"][img_idx][2]
-        acc /= model_num
-        res = []
-        res.append(event[0]["result"][img_idx][0])
-        res.append(event[0]["result"][img_idx][1])
-        res.append(acc)
-
-        result.append(res)
+    bucket = s3.Bucket(BUCKET_NAME)
+    filenames = [file.key for file in bucket.objects.all() if len(file.key.split('/')[1]) > 1]
 
     return {
-        'result': result
+        'file_list': filenames,
+        'batchsize': batchsize
     }
+
