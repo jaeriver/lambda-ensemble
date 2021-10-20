711dc7233fb671a84c55b561d063802504276b43
manchann <chl8273@naver.com>
2021-10-21 00:39:02 +0900

    use dynamodb

--- test.py
+++ test.py
@@ -8,33 +8,30 @@ import time
 from decimal import Decimal
 
 bucket_name = 'imagenet-sample'
-
-s3_path = 'imagenet-sample-images/'
-local_path = '/tmp/'
-
 model_path = 'model/mobilenet_v2'
 model = load_model(model_path, compile=True)
 
+s3 = boto3.resource('s3')
 # def download_image(object_path, file_path):
 #     s3 = boto3.client('s3')
 #     s3.download_file(bucket_name, object_path, file_path)
 
 
-table_name = 'lambda-ensemble'
-region_name = 'us-west-2'
-dynamodb = boto3.resource('dynamodb', region_name=region_name)
-table = dynamodb.Table(table_name)
+# table_name = 'lambda-ensemble'
+# region_name = 'us-west-2'
+# dynamodb = boto3.resource('dynamodb', region_name=region_name)
+# table = dynamodb.Table(table_name)
 
 
-def upload_dynamodb(acc):
-    response = table.put_item(
-        Item={
-            'model_name': 'mobilenet_v2',
-            'case_num': str(time.time()),
-            'test': acc
-        }
-    )
-    return response
+# def upload_dynamodb(acc):
+#     response = table.put_item(
+#         Item={
+#             'model_name': 'mobilenet_v2',
+#             'case_num': str(time.time()),
+#             'test': acc
+#         }
+#     )
+#     return response
 
 def read_image_from_s3(filename):
     bucket = s3.Bucket(bucket_name)
@@ -48,9 +45,6 @@ def read_image_from_s3(filename):
 def filenames_to_input(file_list, batchsize):
     imgs = []
     for i in range(batchsize):
-        # do something!!
-
-        # download_image(file_list[i], local_path + file_name)
         img = read_image_from_s3(file_list[i])
         img = img.resize((224, 224), Image.ANTIALIAS)
         img = np.array(img)
@@ -67,10 +61,12 @@ def filenames_to_input(file_list, batchsize):
 def inference_model(batch_imgs):
     pred_start = time.time()
     result = model.predict(batch_imgs)
-    result = np.round(result.astype(np.float64), 8)
-    result = result.tolist()
     pred_time = time.time() - pred_start
+
+    result = np.round(result.astype(np.float64), 4)
+    result = result.tolist()
     result = json.dumps(result)
+
     return result, pred_time
 
 
@@ -81,9 +77,7 @@ def lambda_handler(event, context):
     batch_imgs = filenames_to_input(file_list, batch_size)
     total_start = time.time()
     result, pred_time = inference_model(batch_imgs)
-    # upload_dynamodb(result)
     total_time = time.time() - total_start
-
     return {
         'result': result,
         'total_time': total_time,
@@ -91,7 +85,6 @@ def lambda_handler(event, context):
     }
 
 
-s3 = boto3.resource('s3')
 batchsize = 3
 bucket = s3.Bucket(bucket_name)
 filenames = [file.key for file in bucket.objects.all() if 'JPEG' in file.key]
